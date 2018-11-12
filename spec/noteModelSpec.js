07cd1e20145f2b4de430d93e1477b91d9e7f4a24
Caitlin Cooling <ccooling97@gmail.com>
2018-11-12 16:09:47 +0000

    Update note model spec to use equals

--- spec/noteModelSpec.js
+++ spec/noteModelSpec.js
@@ -8,8 +8,8 @@ function newNoteIsAnInstanceOfNote() {
 
 function newNoteTakeTextWhenIntialized() {
   var newNote = new Note("This is a note")
-  assert.isTrue("Setting the text of a new note",
-    newNote.getText() === "This is a note")
+  assert.equals("Setting the text of a new note",
+    newNote.getText(), "This is a note")
 }
 
 newNoteIsAnInstanceOfNote()
