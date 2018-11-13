2091e34b013390eb36d8bdaca3597a99a928ab62
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-13 16:10:55 +0000

    update noteController

--- src/noteController.js
+++ src/noteController.js
@@ -1,1 +1,13 @@
-document.getElementById('app').innerHTML = "howdy";
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    this.noteList.add("Favourite drink: seltzer");
+
+    this.noteListView = new NoteListView(this.noteList);
+  }
+
+  var noteController = new NoteController(new NoteList());
+
+  document.getElementById('app').innerHTML = noteController.noteListView.renderView();
+
+})(this);
